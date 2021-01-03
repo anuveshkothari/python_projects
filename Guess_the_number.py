10b9accd3dccdc3b2395eb780e7902a9e5a3fbc0
Anuvesh Kothari <anuveshkothari@gmail.com>
2021-01-03 18:02:39 +0530

    Guess the number

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -15,27 +15,30 @@ def guess_the_number_user(num):
 
 
 def guess_the_number_computer(num):
-    print("Hey computer!! Guess a number between 1 and 100")
+    print(f"Hey computer!! Guess a number between 1 and {num}")
     low = 1
     high = num
     feedback = ''
     while feedback.lower() != 'c':
-        guess_number = int(randint(low, high))
+        if low != high:
+            guess_number = int(randint(low, high))
+        else:
+            guess_number = low # or it can be high b/c both are same
         feedback = input(f"Is the {guess_number} is too low(l) or too high(h) or correct(c)??")
         if feedback.lower() == 'l'.lower():
             low = guess_number + 1
         elif feedback.lower() == 'h':
             high = guess_number - 1
 
-    print("Yay, congrats!! you have guessed the number correctly!!")
+    print(f"Yay, congrats!! you have guessed the number{guess_number} correctly!!")
 
 
 def main():
     game = int(input("If you want to guess a number, press 1\nYou want computer to guess a number, press 2: "))
     if game == 1:
-        guess_the_number_user(100)
+        guess_the_number_user(1000)
     elif game == 2:
-        guess_the_number_computer(100)
+        guess_the_number_computer(1000)
     else:
         print("Invalid input!!")
 
