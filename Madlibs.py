acb82289e2c008d02fb647e782eea4c48071e14b
Anuvesh Kothari <anuveshkothari@gmail.com>
2021-01-03 17:29:16 +0530

    Madlib games

--- Madlibs.py
+++ Madlibs.py
@@ -1,12 +1,3 @@
-"""
-String concatination (how to put string together)
-for eg: we want to say something like " Kindly check my profile on _______
-"""
-# site = "Linkdin"
-# number of ways to append a string
-# print("Kindly check my profile on "+ site)
-# print("Kindly check my profile on {}".format(site))
-# print(f"Kindly check my profile on {site}")
 
 from Madlib import code,Happy,Hunger_games,Zombie
 import random
