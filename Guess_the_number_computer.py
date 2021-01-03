0244e4431593f7828dca4248bbaa2581844553d3
Anuvesh Kothari <anuveshkothari@gmail.com>
2021-01-03 17:18:39 +0530

    Guess the number computer

---
+++ Guess_the_number_computer.py
@@ -0,0 +1,17 @@
+from random import randint
+
+def Guess_the_number(num):
+    random_number = randint(1,num)
+    guess_number = 0
+    while guess_number != random_number:
+        guess_number = int(input(f"Guess the number between 1 to {num}: "))
+        if guess_number > random_number:
+            print("Sorry! guess again, too high")
+        elif guess_number < random_number:
+            print("Sorry! guess again, too low")
+        else:
+            print("Wow! you guessed the number correctly")
+
+
+if __name__ == '__main__':
+    Guess_the_number(10)
